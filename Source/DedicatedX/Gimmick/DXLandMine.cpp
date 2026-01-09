b2021f100d3bb76c45997badbef7cfd247f0fde8
SangBeenKim <sb.kim03699@gmail.com>
2026-01-09 12:49:23 +0900

    feat : 지뢰 액터 폭발 이펙트 추가

--- Source/DedicatedX/Gimmick/DXLandMine.cpp
+++ Source/DedicatedX/Gimmick/DXLandMine.cpp
@@ -1,7 +1,7 @@
 #include "Gimmick/DXLandMine.h"
 #include "Components/BoxComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
-
+#include "Particles/ParticleSystemComponent.h"
 
 ADXLandMine::ADXLandMine()
 {
@@ -17,6 +17,9 @@ ADXLandMine::ADXLandMine()
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 	Mesh->SetupAttachment(BoxCollision);
 
+	Particle = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Particle"));
+	Particle->SetupAttachment(GetRootComponent());
+	Particle->SetAutoActivate(false);
 }
 
 void ADXLandMine::BeginPlay()
@@ -64,6 +67,10 @@ void ADXLandMine::BeginPlay()
 		}
 	}
 	
+	if (!OnActorBeginOverlap.IsAlreadyBound(this, &ThisClass::OnLandMineBeginOverlap))
+	{
+		OnActorBeginOverlap.AddDynamic(this, &ThisClass::OnLandMineBeginOverlap);
+	}
 }
 
 void ADXLandMine::EndPlay(const EEndPlayReason::Type EndPlayReason)
@@ -78,4 +85,61 @@ void ADXLandMine::EndPlay(const EEndPlayReason::Type EndPlayReason)
 		FLinearColor::Green,
 		5.f
 	);
+}
+
+void ADXLandMine::OnLandMineBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (HasAuthority())
+	{
+		UKismetSystemLibrary::PrintString(
+			this,
+			FString::Printf(TEXT("Run on server.")),
+			true,
+			true,
+			FLinearColor::Green,
+			5.f
+		);
+
+		MulticastRPCSpawnEffect();
+	}
+	else
+	{
+		APawn* OwnerPawn = Cast<APawn>(GetOwner());
+		if (IsValid(OwnerPawn))
+		{
+			if (OwnerPawn->IsLocallyControlled())
+			{
+				UKismetSystemLibrary::PrintString(
+					this,
+					FString::Printf(TEXT("Run on owning client.")),
+					true,
+					true,
+					FLinearColor::Green,
+					5.f
+				);
+			}
+			else
+			{
+				UKismetSystemLibrary::PrintString(
+					this,
+					FString::Printf(TEXT("Run on other client.")),
+					true,
+					true,
+					FLinearColor::Green,
+					5.f
+				);
+			}
+		}
+	}
+}
+
+void ADXLandMine::MulticastRPCSpawnEffect_Implementation()
+{
+	UWorld* World = GetWorld();
+	if (!IsValid(World)) return;
+
+	if (World->GetNetMode() != NM_DedicatedServer)
+	{
+		Particle->Activate(true);
+	}
 }
--- Source/DedicatedX/Gimmick/DXLandMine.h
+++ Source/DedicatedX/Gimmick/DXLandMine.h
@@ -7,6 +7,7 @@
 class USceneComponent;
 class UBoxComponent;
 class UStaticMeshComponent;
+class UParticleSystemComponent;
 
 UCLASS()
 class DEDICATEDX_API ADXLandMine : public AActor
@@ -19,10 +20,18 @@ public:
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 private:
+	UFUNCTION()
+	void OnLandMineBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	UFUNCTION(NetMulticast, Unreliable)
+	void MulticastRPCSpawnEffect();
+
+private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess))
 	TObjectPtr<USceneComponent> SceneRoot;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess))
 	TObjectPtr<UBoxComponent> BoxCollision;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess))
-	TObjectPtr <UStaticMeshComponent> Mesh;
+	TObjectPtr<UStaticMeshComponent> Mesh;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess))
+	TObjectPtr<UParticleSystemComponent> Particle;
 };
