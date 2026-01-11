c2998336c7c14b2db7f18863b0537e1a2454b7ae
SangBeenKim <sb.kim03699@gmail.com>
2026-01-11 23:09:46 +0900

    chore : Build.cs "UMG" 추가

--- Source/DedicatedX/DedicatedX.Build.cs
+++ Source/DedicatedX/DedicatedX.Build.cs
@@ -9,7 +9,10 @@ public class DedicatedX : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] 
-		{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+		{ 
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+
+			"UMG",
 
 		});
 
